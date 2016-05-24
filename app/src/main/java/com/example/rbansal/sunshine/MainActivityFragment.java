1d6e120fa53652fc21b7b72f106fcd512546d979
rbansalrahul6 <rbansalrahul6@gmail.com>
2016-05-24 16:06:18 +0530

    added intent for launching DetailActivity

--- app/src/main/java/com/example/rbansal/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/rbansal/sunshine/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package com.example.rbansal.sunshine;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -15,7 +16,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -110,9 +110,10 @@ public class MainActivityFragment extends Fragment {
             public void onItemClick(AdapterView<?> adapterView,View view, int position, long id) {
                 //get forecast string to be displayed in toast
                 String forecast = mForecastAdapter.getItem(position);
-                //initializing and displaying a toast
-                Toast.makeText(getActivity().getApplicationContext(), forecast, Toast.LENGTH_SHORT).show();
-                Log.v("toast","clicked");
+                //initializing and starting an intent
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
 
             }
         });
