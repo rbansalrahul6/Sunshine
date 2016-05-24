aec38d331a98e4799fef06b6106ec60d94a19c66
rbansalrahul6 <rbansalrahul6@gmail.com>
2016-05-24 21:29:40 +0530

    change location in settings

--- app/src/main/java/com/example/rbansal/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/rbansal/sunshine/MainActivityFragment.java
@@ -1,9 +1,11 @@
 package com.example.rbansal.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -67,7 +69,10 @@ public class MainActivityFragment extends Fragment {
 //        }
         if (id == R.id.action_refresh) {
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("94043");
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                       String location = prefs.getString(getString(R.string.pref_location_key),
+                                       getString(R.string.pref_location_default));
+            weatherTask.execute(location);
             return true;
         }
 
